3f2a74c63067d4a9eb493446d24aefe2e8846fad
Sanglyit <Gliztlysang3@gmail.com>
2023-03-07 13:59:57 +0700

    ver 1.5

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 // server.js
-//console.log('May Node be with you')
-
 const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
-
 const MongoClient = require('mongodb').MongoClient
-
 const connectionString = 'mongodb+srv://sa:2345@cluster0.syir52w.mongodb.net/?retryWrites=true&w=majority'
 
 // (0) CONNECT: server -> connect -> MongoDB Atlas 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
-        console.log('Connected to Database')
-        
-        // (1a) CREATE: client -> create -> database -> 'star-wars-quotes'
-        // -> create -> collection -> 'quotes'
+        console.log('Connected to Database')        
+        // (1a) CREATE: client -> create -> database -> 'vn-toys'
+        // -> create -> collection -> 'toys'
         const db = client.db('vn-toys')
-        const quotesCollection = db.collection('toys')
+        const toysCollection = db.collection('toys')
         
         // To tell Express to EJS as the template engine
         app.set('view engine', 'ejs') 
@@ -32,7 +27,7 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
         app.use(bodyParser.json())
 
         // (2) READ: client -> browser -> url 
-        // -> server -> '/' -> collection -> 'quotes' -> find() 
+        // -> server -> '/' -> collection -> 'toys' -> find() 
         // -> results -> index.ejs -> client
         app.get('/', (req, res) => {
             db.collection('toys').find().toArray()
@@ -47,22 +42,18 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
                 })
                 .catch(/* ... */)
         })
-
         // (1b) CREATE: client -> index.ejs -> data -> SUBMIT 
-        // -> post -> '/quotes' -> collection -> insert -> result
+        // -> post -> '/toys' -> collection -> insert -> result
         app.post('/toys', (req, res) => {
-            quotesCollection.insertOne(req.body)
-            .then(result => {
-                
+            toysCollection.insertOne(req.body)
+            .then(result => { 
                 // results -> server -> console
                 console.log(result)
-
                 // -> redirect -> '/'
                 res.redirect('/')
              })
             .catch(error => console.error(error))
         })
-        
         // server -> listen -> port -> 3000
         app.listen(3000, function() {
             console.log('listening on 3000')
